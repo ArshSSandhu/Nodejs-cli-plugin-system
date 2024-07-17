afda42ea0fbeb9cc652b3f9414dfea66ddfa9b7d
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-07-17 13:37:15 -0400

    added index.ts file

---
+++ index.ts
@@ -0,0 +1,23 @@
+import TextCLI from './textCLI';
+import PluginManager from './pluginManager';
+
+const manager = new PluginManager(__dirname);
+
+// Register additional plugins
+manager.registerPlugin({
+  name: 'colors-plugin',
+  packageName: './colorsPlugin',
+  isRelative: true,
+});
+
+manager.registerPlugin({
+  name: 'passwordify-plugin',
+  packageName: './passwordifyPlugin',
+  isRelative: true,
+  options: {
+    symbol: '*'
+  }
+});
+
+const cli = new TextCLI(manager);
+cli.displayPrompt();
