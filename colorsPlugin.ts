3f926406afc1dc6165c676afa32eef045bb955f5
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-07-17 13:35:36 -0400

    added colorsPlugin.ts file

---
+++ colorsPlugin.ts
@@ -0,0 +1,10 @@
+import { TextPlugin } from './types';
+import colors from 'colors';
+
+class ColorsPlugin extends TextPlugin {
+  transformText(text: string): string {
+    return colors.rainbow(text);
+  }
+}
+
+export default ColorsPlugin;
