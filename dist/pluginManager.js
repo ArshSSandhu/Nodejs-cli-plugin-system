024f56087d3f811947b606ea03fac9fad7031a28
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-07-20 20:27:18 -0400

    runtime

--- dist/pluginManager.js
+++ dist/pluginManager.js
@@ -1,9 +1,29 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
+const path = __importStar(require("path"));
 //import requireModule from './node_modules/require-module'; // Install this package if not already
 class PluginManager {
     constructor(basePath) {
@@ -19,7 +39,7 @@ class PluginManager {
         }
         try {
             // Try to load the plugin
-            const packageContents = plugin.isRelative ? require(path_1.default.join(this.path, plugin.packageName)) : require(plugin.packageName);
+            const packageContents = plugin.isRelative ? require(path.join(this.path, plugin.packageName)) : require(plugin.packageName);
             this.addPlugin(plugin, packageContents);
         }
         catch (error) {
--- dist/textCLI.js
+++ dist/textCLI.js
@@ -3,41 +3,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer"));
+const inquirer_1 = __importDefault(require("inquirer")); // Importing inquirer library for interactive prompts
+// Class definition for the CLI application
 class TextCLI {
     constructor(pluginManager) {
-        this.pluginManager = pluginManager;
-        // Register the default behavior plugin
+        this.pluginManager = pluginManager; // Assigning the passed PluginManager instance to this.pluginManager
+        // Registering a default behavior plugin named 'echo-plugin'
         this.pluginManager.registerPlugin({
-            name: 'echo-plugin',
-            packageName: './echoPlugin',
-            isRelative: true,
+            name: 'echo-plugin', // Plugin name
+            packageName: './echoPlugin', // Package location (relative path)
+            isRelative: true // Indicates that the path is relative to the current file
         });
     }
+    // Method to display prompts and handle user input
     displayPrompt() {
         const pluginChoices = [];
+        // Iterating over the list of registered plugins and extracting their names
         this.pluginManager.listPluginList().forEach((plugin) => {
-            pluginChoices.push(plugin.name);
+            pluginChoices.push(plugin.name); // Adding each plugin's name to pluginChoices array
         });
-        // Define the questions with explicit types
         const questions = [
             {
                 type: 'input',
                 name: 'text',
-                message: 'What text do you want to transform?',
+                message: 'What text do you want to transform?', // Prompt message for text input
             },
             {
                 type: 'list',
                 name: 'pluginName',
-                message: 'What plugin do you want to execute?',
-                choices: pluginChoices,
+                message: 'What plugin do you want to execute?', // Prompt message for plugin selection
+                choices: pluginChoices, // Available plugin choices obtained earlier
             },
         ];
-        inquirer_1.default.prompt(questions).then((answer) => {
-            // Execute the plugin
-            const textPlugin = this.pluginManager.loadPlugin(answer.pluginName);
-            console.log(`This is the transformed result for ${answer.text}: ${textPlugin.transformText(answer.text)}`);
+        inquirer_1.default
+            .prompt(questions)
+            .then((answers) => {
+            // Destructure the answer object to match ITextSelectedChoice
+            const { text, pluginName } = answers;
+            // Execute the selected plugin
+            const textPlugin = this.pluginManager.loadPlugin(pluginName); // Loading the selected plugin
+            console.log(`This is the transformed result for ${text}: ${textPlugin.transformText(text)}`); // Logging the transformed result
         });
     }
 }
-exports.default = TextCLI;
+exports.default = TextCLI; // Exporting the TextCLI class as default
