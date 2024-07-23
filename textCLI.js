f78cd19acf7a2721af171638aae2682d668cbff7
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-07-23 17:34:24 -0400

    documentation fix

--- textCLI.js
+++ textCLI.js
@@ -3,55 +3,55 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer")); // Importing inquirer library for interactive prompts
-// Class definition for the CLI application
+const inquirer_1 = __importDefault(require("inquirer")); 
+
 class TextCLI {
     constructor(pluginManager) {
-        this.pluginManager = pluginManager; // Assigning the passed PluginManager instance to this.pluginManager
-        // Registering a default behavior plugin named 'echo-plugin'
-     this.pluginManager.registerPlugin({
-           name: 'echo-plugin', // Plugin name
-           packageName: './echoPlugin', // Package location (relative path)
-           isRelative: true // Indicates that the path is relative to the current file
-        });
-
-        // Register new Uppercase Plugin
+        this.pluginManager = pluginManager; 
+      
     this.pluginManager.registerPlugin({
-       name: 'uppercase-plugin',
-       packageName: './src/upperCasePlugin',
+        name: 'echo-plugin', // Plugin name
+        packageName: './echoPlugin', // Package location (relative path)
+        isRelative: true // Indicates that the path is relative to the current file
+                                        });
+
+    // Register new Uppercase Plugin
+    this.pluginManager.registerPlugin({
+        name: 'uppercase-plugin',
+        packageName: './src/upperCasePlugin',
         isRelative: true,
-      });
+                                        });
 
     }
-    // Method to display prompts and handle user input
+   
     displayPrompt() {
         const pluginChoices = [];
-        // Iterating over the list of registered plugins and extracting their names
+      
         this.pluginManager.listPluginList().forEach((plugin) => {
-            pluginChoices.push(plugin.name); // Adding each plugin's name to pluginChoices array
+            pluginChoices.push(plugin.name); 
         });
         const questions = [
             {
                 type: 'input',
                 name: 'text',
-                message: 'What text do you want to transform?', // Prompt message for text input
+                message: 'What text do you want to transform?', 
             },
             {
                 type: 'list',
                 name: 'pluginName',
-                message: 'What plugin do you want to execute?', // Prompt message for plugin selection
-                choices: pluginChoices, // Available plugin choices obtained earlier
+                message: 'What plugin do you want to execute?',
+                choices: pluginChoices, 
             },
         ];
         inquirer_1.default
             .prompt(questions)
             .then((answers) => {
-            // Destructure the answer object to match ITextSelectedChoice
+         
             const { text, pluginName } = answers;
-            // Execute the selected plugin
-            const textPlugin = this.pluginManager.loadPlugin(pluginName); // Loading the selected plugin
-            console.log(`This is the transformed result for ${text}: ${textPlugin.transformText(text)}`); // Logging the transformed result
+            
+            const textPlugin = this.pluginManager.loadPlugin(pluginName); 
+            console.log(`This is the transformed result for ${text}: ${textPlugin.transformText(text)}`); 
         });
     }
 }
-exports.default = TextCLI; // Exporting the TextCLI class as default
+exports.default = TextCLI; 
