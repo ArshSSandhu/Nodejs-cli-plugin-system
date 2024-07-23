1d9380f425c0b8984735fc150a0dbd369ddcd299
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-07-23 17:35:15 -0400

    removed unwanted code from index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,5 @@ import PluginManager from './pluginManager';
 
 const manager = new PluginManager(__dirname);
 
-// Register additional plugins
-//manager.registerPlugin({
-//  name: 'colors-plugin',
-  //packageName: './colorsPlugin',
-  //isRelative: true,
-//});
-
-//manager.registerPlugin({
-  //name: 'passwordify-plugin',
-  //packageName: './passwordifyPlugin',
-  //isRelative: true,
-  //options: {
-    //symbol: '*'
-  //}
-//});
-
 const cli = new TextCLI(manager);
 cli.displayPrompt();
