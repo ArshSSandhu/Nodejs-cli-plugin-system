5710912bd07291987faf97870a8ca937f2e615de
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-07-23 17:34:49 -0400

    removed unwanted code from index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var textCLI_1 = require("./textCLI");
 var pluginManager_1 = require("./pluginManager");
 var manager = new pluginManager_1.default(__dirname);
-// Register additional plugins
-//manager.registerPlugin({
- //   name: 'colors-plugin',
- //   packageName: './colorsPlugin',
- //   isRelative: true,
-//});
-//manager.registerPlugin({
-  //  name: 'passwordify-plugin',
-  //  packageName: './passwordifyPlugin',
-  //  isRelative: true,
- //   options: {
-  //      symbol: '*'
-  //  }
-//});
+
 var cli = new textCLI_1.default(manager);
 cli.displayPrompt();
